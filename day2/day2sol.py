e04cda469cd25860ad3e20d109d51d580b6baeb8
Tony Ferrell <anf@microsoft.com>
2020-12-02 22:02:21 -0800

    Day2 Part2

--- day2/day2sol.py
+++ day2/day2sol.py
@@ -1,17 +1,38 @@
-with open('input.txt') as data:
-    valid = 0
-    for line in data:
-        char_range, char, password = line.split(' ')
-        min, max = char_range.split('-')
-        min, max = int(min), int(max)
-        char = char[0]
+def part1():
+    with open('input.txt') as data:
+        valid = 0
+        for line in data:
+            char_range, char, password = line.split(' ')
+            min, max = char_range.split('-')
+            min, max = int(min), int(max)
+            char = char[0]
 
-        count = 0
-        for c in password:
-           if c == char:
-               count += 1
-            
-        if min <= count and count <= max:
-            print(password, "is valid")
-            valid += 1
-    print("There are {} valid passwords".format(valid))
+            count = 0
+            for c in password:
+                if c == char:
+                    count += 1
+                    
+                if min <= count and count <= max:
+                    print(password, "is valid")
+                    valid += 1
+        print("There are {} valid passwords".format(valid))
+
+
+def part2():
+    with open('input.txt') as data:
+        valid = 0
+        for line in data:
+            char_range, char, password = line.split(' ')
+            min, max = char_range.split('-')
+            min, max = int(min), int(max)
+            char = char[0]
+
+            count = 0
+            first, second = password[min-1] == char, password[max-1] ==char
+                
+            if first ^ second:
+                print(password, "is valid")
+                valid += 1
+        print("There are {} valid passwords".format(valid))
+
+part2()
