91978bcbf95c50dc5620260c9b069a95040bf396
Tony Ferrell <anf@microsoft.com>
2020-12-02 22:57:37 -0800

    I really broke it

--- day3/day3sol.py
+++ day3/day3sol.py
@@ -1,4 +1,4 @@
-
+from functools import reduce
 class InfinteWidthMap:
 
     def __init__(self, filename):
@@ -6,11 +6,16 @@ class InfinteWidthMap:
         self.open_char = '.'
         self._base_map = []
         self.height = 0
+        self.width = 0
 
         with open(filename) as map:
             for row_raw in map:
+                if not row_raw:
+                    continue
+
                 row = [m for m in row_raw]
-                self.width = len(row)
+                # Better to have an out of bounds error than a subtle shortening of rows.
+                self.width = max(len(row), self.width)
                 self._base_map.append(row)
             
             self.height = len(self._base_map)
@@ -22,7 +27,7 @@ class InfinteWidthMap:
             raise Exception("Too far!")
 
         a_char = self._base_map[y][a_x] 
-        print("Getting ({}, {} = {})".format(a_x+1, y+1, a_char))
+        # print("Getting ({}, {} = {})".format(a_x+1, y+1, a_char))
         return a_char
     
     def is_tree(self, x, y):
@@ -33,14 +38,23 @@ class InfinteWidthMap:
         return self.height
 
 def part1():
-    m = InfinteWidthMap('input.txt')
-    tree_count = 0
-    x = 0
-    for y in range(1, m.get_height()):
-        x += 3
-        if m.is_tree(x, y):
-            tree_count += 1
-    
-    print("You hit {} trees".format(tree_count))
+    m = InfinteWidthMap('test.txt')
+    traverses = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+    a_t = []
+    for d_x, d_y in traverses:
+        tree_count = 0
+        x = 0
+        for y in range(0, m.get_height(), d_y):
+            x += d_x
+            if m.is_tree(x, y):
+                tree_count += 1
+
+        a_t.append(tree_count)
+        print("You ({}, {}) hit {} trees".format(d_x, d_y, tree_count))
+
+        total = reduce(lambda x,y: x * y, a_t)
+
+    print("The total of {} is: {}".format(a_t, total))
+
 
 part1()
