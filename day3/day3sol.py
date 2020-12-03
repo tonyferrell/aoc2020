df0ae92a46e621489414daf30cc8fd912c91853c
Tony Ferrell <anf@microsoft.com>
2020-12-02 23:00:19 -0800

    Day3 Part 2 failing, but mostly correct

--- day3/day3sol.py
+++ day3/day3sol.py
@@ -6,16 +6,14 @@ class InfinteWidthMap:
         self.open_char = '.'
         self._base_map = []
         self.height = 0
-        self.width = 0
 
         with open(filename) as map:
             for row_raw in map:
                 if not row_raw:
+                    print("Skipping", row_raw)
                     continue
-
                 row = [m for m in row_raw]
-                # Better to have an out of bounds error than a subtle shortening of rows.
-                self.width = max(len(row), self.width)
+                self.width = len(row)
                 self._base_map.append(row)
             
             self.height = len(self._base_map)
@@ -38,21 +36,21 @@ class InfinteWidthMap:
         return self.height
 
 def part1():
-    m = InfinteWidthMap('test.txt')
+    m = InfinteWidthMap('input.txt')
     traverses = [(1,1), (3,1), (5,1), (7,1), (1,2)]
     a_t = []
     for d_x, d_y in traverses:
         tree_count = 0
         x = 0
-        for y in range(0, m.get_height(), d_y):
+        for y in range(1, m.get_height(), d_y):
             x += d_x
             if m.is_tree(x, y):
                 tree_count += 1
 
         a_t.append(tree_count)
-        print("You ({}, {}) hit {} trees".format(d_x, d_y, tree_count))
+        print("You hit {} trees".format(tree_count))
 
-        total = reduce(lambda x,y: x * y, a_t)
+        total = reduce(lambda x,y: x*y, a_t)
 
     print("The total of {} is: {}".format(a_t, total))
 
