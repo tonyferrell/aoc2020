a1b1c16081d08284336c3b68a699dc3b4a3e3d2e
Tony Ferrell <anf@microsoft.com>
2020-12-14 10:51:26 -0800

    Just some comments

--- day10/day10sol.py
+++ day10/day10sol.py
@@ -3,16 +3,23 @@ with open("input.txt") as data:
     for line in data:
         lines.append(int(line))
 
+# Add starting value
 lines.append(0)
 lines.sort()
+# Add "power adapter" value
 lines.append(lines[-1]+3)
 
+# Memorize how many ways there are to reach the end from each value
 counts = [0] * len(lines)
 counts[-1] = 1
 
 for i in range(0, len(lines)):
+    # Start at the last value and work forward
     idx = len(lines) - i - 1
     curr_val = lines[idx]
+
+    # Search forward until you find a value that is no longer within 3 jolts, 
+    # and count how many ways those could reach the end.
     count = 0
     for s_i in range(idx, len(lines)):
         n = lines[s_i]
