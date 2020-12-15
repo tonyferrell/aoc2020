684c72812b2502cb4e06263511d4ef423bf81a9a
Tony Ferrell <anf@microsoft.com>
2020-12-14 22:08:55 -0800

    Day14, Part1

--- day14/day14sol.py
+++ day14/day14sol.py
@@ -1,4 +1,5 @@
 import re
+
 zero_mask = 0
 one_mask = 0
 
@@ -10,7 +11,7 @@ def exec_mask(line: str):
     print("This is mask", mask)
     one_mask = 0
     zero_mask = 0
-    for b in line:
+    for b in mask.strip():
         one_mask = one_mask << 1
         zero_mask = zero_mask << 1
 
@@ -23,19 +24,37 @@ def exec_mask(line: str):
         elif b == 'X':
             one_mask |= 0
             zero_mask |= 1
+        else:
+            raise Exception("Unexpected value: ", b)
 
-
+mem_loc = re.compile("mem\[(\d+)\]")
 def exec_mem(line: str):
+    verbose = False
     loc, val = line.split(" = ")
+    loc_str = mem_loc.match(loc)
+    loc_i = int(loc_str[1])
+    val_i = int(val)
+    if verbose:
+        print("Val: {:b}".format(val_i))
+    val_i &= zero_mask
+    if verbose:
+        print("Val: {:b}".format(val_i))
+    val_i |= one_mask
+    if verbose:
+        print("Val: {:b}".format(val_i))
+
+    print("mem[{}] = {}".format(loc_i, val_i))
+    mem[loc_i] = val_i
 
     return
 
-with open('test.txt') as data:
+with open('input.txt') as data:
     for line in data:
         if line.startswith('mem'):
             exec_mem(line)
         elif line.startswith('mask'):
             exec_mask(line)
+            print("One Mask: {:b}".format(one_mask))
+            print("Zero Mask: {:b}".format(zero_mask))
 
-print("One Mask: {:b}".format(one_mask))
-print("Zero Mask: {:b}".format(zero_mask))
+print("Memory Total: {}".format(sum([val for mem, val in mem.items()])))
