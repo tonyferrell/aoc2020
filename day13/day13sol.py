8df7dc5ea52b8066e9d0967cddd77a3a7a7550e8
Tony Ferrell <anf@microsoft.com>
2020-12-14 22:08:34 -0800

    Day13, Part2

--- day13/day13sol.py
+++ day13/day13sol.py
@@ -1,16 +1,50 @@
 with open('input.txt') as data:
     departure = int(next(data))
     r = next(data)
-    times = [int(x) for x in r.split(",") if x != 'x']
+    times = [int(x) if x != 'x' else 0 for x in r.split(",")]
 
+
+num = d_n = times[0]
+# num = 100000000000000
+# num = 102563359500841
+# d_n = max(times)
+adj = times.index(d_n)
+
+while num == 0 or num % d_n != 0:
+    num += 1
+
+print("Starting at {}, delta of {}".format(num, d_n))
+
+times = list(filter(lambda x: x[1] > 0, list(enumerate(times))))
 print(times)
 
-min_t = max(times)
-min_mod = min_t
-for t in times:
-    mod = t - (departure % t)
-    if mod < min_mod:
-        min_t = t
-        min_mod = mod
+c = 0
+while True:
+    c += 1
+    if c % 500000 == 0:
+        print("Checking", num)
+    success = True
 
-print(min_t *(min_t - (departure % min_t)))
+    match = 1
+    for i, v in times:
+        if v == 0:
+            continue
+
+        c = (num - adj + i)
+
+
+        # print("Trying {}: ({}) {} != 0".format(num, v, c % v))
+        if c % v != 0:
+            success = False
+            break
+        
+        match *= v
+        if match > d_n:
+            d_n = match
+            print("New delta", d_n)
+
+    if success:
+        print("{} was the number!".format(num - adj))
+        break
+    
+    num += d_n
